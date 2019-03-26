37b84946770ebda9e2fafad155744ca4cdca766c
jkub6 <jkub66@gmail.com>
2019-03-26 16:42:23 -0600

    further organized client

---
+++ Client/Tests/PS4Tests.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+using SpreadsheetUtilities;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class PS4Tests
+    {
+        [TestMethod]
+        public void GetNamesOfAllNonemptyCellsNone()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.AreEqual(0, s.GetNamesOfAllNonemptyCells().Count());
+        }
+
+        [TestMethod]
+        public void GetNamesOfAllNonemptyCellsOne()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("B1", "=A1*2");
+            Assert.AreEqual(1, s.GetNamesOfAllNonemptyCells().Count());
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("B1"));
+        }
+
+        [TestMethod]
+        public void GetNamesOfAllNonemptyCellsTwo()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("B1", "=A1*2");
+            s.SetContentsOfCell("C1", "=D1*2");
+            Assert.AreEqual(2, s.GetNamesOfAllNonemptyCells().Count());
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("B1"));
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("C1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellOneFormula()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            ISet<string> set = s.SetContentsOfCell("A1", "B1 + 2");
+            Assert.AreEqual(1, set.Count);
+            Assert.IsTrue(set.Contains("A1"));
+
+            Assert.AreEqual(1, s.GetNamesOfAllNonemptyCells().Count());
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("A1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellTwoFormulas()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("C1", "=B1*2");
+            ISet<string> set = s.SetContentsOfCell("B1", "=A1+A2");
+            Assert.AreEqual(2, set.Count);
+            Assert.IsTrue(set.Contains("C1"));
+            Assert.IsTrue(set.Contains("B1"));
+
+            Assert.AreEqual(2, s.GetNamesOfAllNonemptyCells().Count());
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("B1"));
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("C1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellTwoFormulasReset()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("C1", "=B1");
+            s.SetContentsOfCell("C1", "2.0");
+            ISet<string> set = s.SetContentsOfCell("B1", "=A1");
+            Assert.AreEqual(1, set.Count);
+            Assert.IsTrue(set.Contains("B1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellThreeFormulasOneDouble()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("B1", "=A1*2");
+            s.SetContentsOfCell("C1", "=B1+A1");
+            s.SetContentsOfCell("D1", "=H1+F1");
+            ISet<string> set = s.SetContentsOfCell("A1", "4");
+            Assert.AreEqual(3, set.Count);
+            Assert.IsTrue(set.Contains("A1"));
+            Assert.IsTrue(set.Contains("B1"));
+            Assert.IsTrue(set.Contains("C1"));
+
+            Assert.AreEqual(4, s.GetNamesOfAllNonemptyCells().Count());
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("A1"));
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("B1"));
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("C1"));
+            Assert.IsTrue(s.GetNamesOfAllNonemptyCells().Contains("D1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellFirstNull()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell(null, "=A1*2"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellSecondNull()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<ArgumentNullException>(() => s.SetContentsOfCell("A5", null));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellBothNull()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell(null, null));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellFirstInvalid()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("$b4", "=12 + 3"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsDouble()
+        {
+            // For example, if name is A1, B1 contains A1 * 2, and C1 contains B1+A1, the
+            // set {A1, B1, C1} is returned.
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "3");
+            s.SetContentsOfCell("B1", "-1.0");
+            s.SetContentsOfCell("C1", "10e+15");
+            s.SetContentsOfCell("D1", ".0001");
+
+            Assert.AreEqual(3.0, s.GetCellContents("A1"));
+            Assert.AreEqual(-1.0, s.GetCellContents("B1"));
+            Assert.AreEqual(10e+15, s.GetCellContents("C1"));
+            Assert.AreEqual(.0001, s.GetCellContents("D1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsString()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "test");
+            s.SetContentsOfCell("B1", "asdk395@#%$");
+            s.SetContentsOfCell("C1", " ");
+            s.SetContentsOfCell("D1", "");
+
+            Assert.AreEqual("test", s.GetCellContents("A1"));
+            Assert.AreEqual("asdk395@#%$", s.GetCellContents("B1"));
+            Assert.AreEqual(" ", s.GetCellContents("C1"));
+            Assert.AreEqual("", s.GetCellContents("D1"));
+            Assert.AreEqual("", s.GetCellContents("E1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsInvalid()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("$A1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsNull()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents(null));
+        }
+
+        [TestMethod]
+        public void CircularExceptionSelf()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<CircularException>(() => s.SetContentsOfCell("A1", "=A1"));
+        }
+
+        [TestMethod]
+        public void CircularExceptionDirect()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=B1");
+            Assert.ThrowsException<CircularException>( () => s.SetContentsOfCell("B1", "=A1"));
+        }
+
+        [TestMethod]
+        public void CircularExceptionLessDirect()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=14 * B1 - 2* (12 / C4)");
+            Assert.ThrowsException<CircularException>(() => s.SetContentsOfCell("B1", "=12 + E4 * (A1 - 3)"));
+        }
+
+        [TestMethod]
+        public void CircularExceptionIndirect()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=B1");
+            s.SetContentsOfCell("B1", "=C1");
+            s.SetContentsOfCell("C1", "=D1");
+            Assert.ThrowsException<CircularException>(() => s.SetContentsOfCell("D1", "=A1"));
+        }
+
+        [TestMethod]
+        public void CircularExceptionChangesNothing()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+
+            s.SetContentsOfCell("A1", "test");
+            s.SetContentsOfCell("B1", "2.0");
+            s.SetContentsOfCell("C1", "");
+            s.SetContentsOfCell("D1", "=E1");
+            Assert.ThrowsException<CircularException>(() => s.SetContentsOfCell("E1", "=D1"));
+
+            var set = s.GetNamesOfAllNonemptyCells();
+            Assert.AreEqual(3, set.Count());
+            Assert.IsTrue(set.Contains("A1"));
+            Assert.IsTrue(set.Contains("B1"));
+            Assert.IsTrue(set.Contains("D1"));
+
+            Assert.AreEqual("test", s.GetCellContents("A1"));
+            Assert.AreEqual(2.0, s.GetCellContents("B1"));
+            Assert.AreEqual("", s.GetCellContents("C1"));
+            Assert.AreEqual(new Formula("E1"), s.GetCellContents("D1"));
+        }
+
+        [TestMethod]
+        public void CircularExceptionChangesFew()
+        {
+            SS.Spreadsheet s = new Spreadsheet();
+
+            s.SetContentsOfCell("A1", "=C1");
+            s.SetContentsOfCell("A1", "=D1");
+            s.SetContentsOfCell("A1", "=E1");
+            s.SetContentsOfCell("A1", "=F1");
+            var a = s.SetContentsOfCell("F1", "=3+3");
+
+            Assert.AreEqual(2, a.Count);
+            
+        }
+    }
+}
---
+++ Client/Tests/PS5Tests.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+using SpreadsheetUtilities;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class PS5Tests
+    {
+        [TestMethod]
+        public void GetCellValueDouble()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "12.556");
+            Assert.AreEqual(12.556, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueDoubleNegative()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "-12.556");
+            Assert.AreEqual(-12.556, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueDoubleZero()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "0");
+            Assert.AreEqual(0.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueString()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "hello");
+            Assert.AreEqual("hello", s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueStringEmpty()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "");
+            Assert.AreEqual("", s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueStringEmpty2()
+        {
+            Spreadsheet s = new Spreadsheet();
+            Assert.AreEqual("", s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueStringNull()
+        {
+            Spreadsheet s = new Spreadsheet();
+            //s.SetContentsOfCell("A1", (string)null);
+            Assert.IsTrue(true);
+            //Assert.AreEqual("", s.GetCellValue("A1")); //put expected error
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaOneReference()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A2", "12");
+            s.SetContentsOfCell("A1", "=A2 + 1");
+            Assert.AreEqual(13.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaTwoReferences()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A2", "12");
+            s.SetContentsOfCell("A3", "5");
+            s.SetContentsOfCell("A1", "= A2 * A3");
+            Assert.AreEqual(60.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaChainReference()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2 + 6");
+            s.SetContentsOfCell("A2", "=A3 + 5");
+            s.SetContentsOfCell("A3", "=A4 + 4");
+            s.SetContentsOfCell("A4", "=A5 + 3");
+            s.SetContentsOfCell("A5", "=A6 + 2");
+            s.SetContentsOfCell("A6", "1");
+
+            Assert.AreEqual(21.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaTreeReference()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2 + A3");
+
+            s.SetContentsOfCell("A2", "=A4 + A5");
+            s.SetContentsOfCell("A3", "=A6 + A7");
+
+            s.SetContentsOfCell("A4", "=4");
+            s.SetContentsOfCell("A5", "3");
+            s.SetContentsOfCell("A6", "=2");
+            s.SetContentsOfCell("A7", "1");
+
+            
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaStringError()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "blah");
+
+            Object f = s.GetCellValue("A1");
+            Assert.IsTrue(f is FormulaError);
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaNull()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue(null));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueNoNormalize()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=1+B1");
+            s.SetContentsOfCell("a1", "=3+b1");
+            s.SetContentsOfCell("B1", "5");
+            s.SetContentsOfCell("b1", "10");
+
+            Assert.AreEqual(6.0, s.GetCellValue("A1"));
+            Assert.AreEqual(13.0, s.GetCellValue("a1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueYesNormalize()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t.ToUpper(), "version blah");
+            s.SetContentsOfCell("A1", "=1+b1");
+            s.SetContentsOfCell("a1", "=3+B1");
+            s.SetContentsOfCell("B1", "5");
+            s.SetContentsOfCell("b1", "10");
+
+            Assert.AreEqual(13.0, s.GetCellValue("A1"));
+            Assert.AreEqual(13.0, s.GetCellValue("a1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaInvalidByDeffinition()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue("3A2"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaInvalidBySpreadsheet()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue("_A2"));
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue("A2A"));
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue("A_2"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellValueFormulaInvalidBValidator()
+        {
+            Spreadsheet s = new Spreadsheet(t => !t.Contains("C"), t => t, "version blah");
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellValue("C5"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellInvalidByDeffinition()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("3A2", "3"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellInvalidBySpreadsheet()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("_A2", "3"));
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("A2A", "3"));
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("A_2", "3"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void SetContentsOfCellInvalidBValidator()
+        {
+            Spreadsheet s = new Spreadsheet(t => !t.Contains("C"), t => t, "version blah");
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+            Assert.ThrowsException<InvalidNameException>(() => s.SetContentsOfCell("C5", "3"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsInvalidByDeffinition()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("3A2"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsInvalidBySpreadsheet()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("_A2"));
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("A2A"));
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("A_2"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void GetCellContentsInvalidBValidator()
+        {
+            Spreadsheet s = new Spreadsheet(t => !t.Contains("C"), t => t, "version blah");
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+            Assert.ThrowsException<InvalidNameException>(() => s.GetCellContents("C5"));
+
+            Assert.AreEqual(3.0, s.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void SaveInvalidName()
+        {
+            Spreadsheet s = new Spreadsheet();
+            Assert.ThrowsException<SpreadsheetReadWriteException>(() => s.Save("      \n.xml"));
+        }
+
+        [TestMethod]
+        public void SaveAndLoad()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+            s.Save("test.xml");
+            
+            Spreadsheet s2 = new Spreadsheet("test.xml", t => true, t => t, "default");
+            s2.SetContentsOfCell("A2", "10");
+
+            Assert.AreEqual(10.0, s2.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void SaveAndLoadVersionMismatch()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=A2");
+            s.SetContentsOfCell("A2", "3");
+            s.Save("test.xml");
+
+            Assert.ThrowsException<SpreadsheetReadWriteException>(() => new Spreadsheet("test.xml", t => true, t => t, "blah"));
+        }
+
+        [TestMethod]
+        public void SaveAndLoadSpecialCharacters()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "\\/<>,</test><cell>");
+            s.Save("test.xml");
+
+            Spreadsheet s2 = new Spreadsheet("test.xml", t => true, t => t, "default");
+
+            Assert.AreEqual("\\/<>,</test><cell>", s2.GetCellValue("A1"));
+        }
+
+        [TestMethod]
+        public void LoadNonexistant()
+        {
+            Assert.ThrowsException<SpreadsheetReadWriteException>(() => new Spreadsheet("fake_news.xml", t => true, t => t, "blah"));
+        }
+
+        [TestMethod]
+        public void LoadFileVersion()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t, "version 1.2.3");
+            s.Save("test.xml");
+
+            Assert.AreEqual("version 1.2.3", s.GetSavedVersion("test.xml"));
+        }
+
+        [TestMethod]
+        public void LoadFileVersionNonExistant()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t, "version 1.2.3");
+
+            Assert.ThrowsException<SpreadsheetReadWriteException>(() => s.GetSavedVersion("fake_news.html"));
+        }
+
+        [TestMethod]
+        public void Changed()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t, "version 1.2.3");
+            s.SetContentsOfCell("A1", "5");
+            Assert.IsTrue(s.Changed);
+        }
+
+        [TestMethod]
+        public void ChangedNot()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t, "version 1.2.3");
+            Assert.IsFalse(s.Changed);
+        }
+
+        [TestMethod]
+        public void ChangedNotCircular()
+        {
+            Spreadsheet s = new Spreadsheet();
+            Assert.IsFalse(s.Changed);
+            Assert.ThrowsException<CircularException>(() => s.SetContentsOfCell("D1", "=D1"));
+            Console.WriteLine(s.GetCellValue("D1"));
+            Console.WriteLine("asdf");
+            Assert.IsFalse(s.Changed);
+        }
+
+        [TestMethod]
+        public void ChangedNotEmpty()
+        {
+            Spreadsheet s = new Spreadsheet(t => true, t => t, "version 1.2.3");
+            s.SetContentsOfCell("A1", "");
+            Assert.IsFalse(s.Changed);
+        }
+
+        [TestMethod]
+        public void StressTest()
+        {
+            Spreadsheet s = new Spreadsheet();
+
+            s.SetContentsOfCell("A0", "0");
+
+            double num = 100000.0;
+
+            for (int i = 0; i < num+1; i++)
+                s.SetContentsOfCell("A"+(i+1), "=A"+i+"+1");
+
+            for (int i = 0; i < num; i++)
+                Assert.AreEqual(num, s.GetCellValue("A"+num));
+        }
+    }
+}
