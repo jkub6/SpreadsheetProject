3fc8d73d721f60c1e6c79a283b67303f9c2aa6f4
AaronGoh <aaronwgoh5@gmail.com>
2019-04-02 15:40:24 -0600

    Grabbed code online for the function of TCP connection

---
+++ ServerAdmin/ServerAdmin/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServerAdmin.Models;
+
+namespace ServerAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(String username, String password, String ipAddress)
+        {
+            ConnectToServer(ipAddress);
+            return View();
+        }
+
+        public void ConnectToServer(String ipAddress)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect(ipAddress, 2112);
+
+                // use the ipaddress as in the server program
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                UTF8Encoding asen = new UTF8Encoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
