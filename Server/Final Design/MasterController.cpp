3a1b8765f8de7d960b3614b859e775d56df02993
Jacob Blomquist <jacobrb95@gmail.com>
2019-04-21 12:34:45 -0600

    Cleaned up Code

--- Server/Final Design/MasterController.cpp
+++ Server/Final Design/MasterController.cpp
@@ -59,12 +59,18 @@ int MasterController::newClientConnected(int socketID)
   //***************************
   //send list of spreadsheets:
   //******************************
+
+
+  
+  
   std::vector<std::string> *list = Utilities::getSpreadsheetList();
   
   nlohmann::json jsonObject;
 
   SocketState * sstate = new SocketState(socketID);
 
+
+  
   jsonObject["type"]="list";
 
   for(int i = 0;i<list->size();i++)
@@ -86,15 +92,6 @@ int MasterController::newClientConnected(int socketID)
   //start thread for listening:
   (*threadpool)[socketID]=new std::thread(&SocketState::socketAwaitData,sstate);
   
-//  threadpool->push_back(new std::thread(&SocketState::socketAwaitData,sstate)
-  
-  
-  /*    while(sstate->isConnected())
-  {
-      sstate->socketAwaitData();
-
-      if(!sstate->isConnected())
-      break;;*/
 
   
   std::vector<std::string> * sdata;
@@ -114,7 +111,6 @@ int MasterController::newClientConnected(int socketID)
 	break;
       
       std::string userRequest = sstate->getSingleMessage();// getCommandsToProcess();
-
       nlohmann::json newCommand;
       
       if(userRequest!="")
@@ -138,7 +134,7 @@ int MasterController::newClientConnected(int socketID)
 		    sendJsonError(sstate);
 		    continue;
 		  }
-
+		
 		if(Utilities::validateUser(username,password)&&desiredSheet!="")
 		  {
 		    userValidated=true;
@@ -158,7 +154,7 @@ int MasterController::newClientConnected(int socketID)
 		std::map<std::string,std::string> *userList = Utilities::getUserList();
 		nlohmann::json response;
 		response["type"]="user";
-
+		
 		for(std::map<std::string,std::string>::iterator it = userList->begin(); it!=userList->end();it++)
 		  {
 		    response["users"].push_back(it->first);
@@ -187,15 +183,15 @@ int MasterController::newClientConnected(int socketID)
 	      {
 		std::cout<<"SPREADSHEET LIST"<<std::endl;
 		std::vector<std::string> * list = Utilities::getSpreadsheetList();
-
+		
 		nlohmann::json response;
 		response["type"]="spread";
-
+		
 		for(std::vector<std::string>::iterator it = list->begin();it!=list->end();it++)
 		  {
 		    response["Sheets"].push_back(*it);
 		  }
-
+		
 		std::cout<<"Spread List:\n"<<response.dump(1);
 		sstate->socketSendData(response.dump(0));
 	      }
@@ -214,19 +210,19 @@ int MasterController::newClientConnected(int socketID)
 	      sendJsonError(sstate);
 	    }
 	}
-
+      
       std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
+      
       
     }
-
   
-
+  
+  
   
   //********************
   //TRANSFER SOCKETSTATE TO SPREADSHEETCONTROLLER
   //**************************
-
+  
   this->spreadsheetController->connectedClient(sstate,desiredSheet);
   
   return 0;
